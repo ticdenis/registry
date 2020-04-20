0c6418e8b27f834a30e8b6424d85899a585f15f6
ticdenis <denisnavarroalcaide@outlook.es>
2020-04-20 21:38:14 +0200

    Upgrade Mongo

--- mongo/initdb.d/init-mongo.sh
+++ mongo/initdb.d/init-mongo.sh
@@ -9,5 +9,10 @@ if [ "$MONGO_INITDB_USERNAME" ] && [ "$MONGO_INITDB_PASSWORD" ]; then
         pwd: $(_js_escape "$MONGO_INITDB_PASSWORD"),
         roles: [ { role: 'readWrite', db: $(_js_escape "$MONGO_INITDB_DATABASE") } ]
     })
+    db.createUser({
+        user: $(_js_escape "$MONGO_INITDB_ROOT_USERNAME"),
+        pwd: $(_js_escape "$MONGO_INITDB_ROOT_PASSWORD"),
+        roles: [ { role: 'readWrite', db: $(_js_escape "$MONGO_INITDB_DATABASE") } ]
+    })
 EOJS
 fi
